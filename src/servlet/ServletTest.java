1fef99c9b4ecd239236f2ceca9d74c0559c03e81
gzr736278296 <13164119626@163.com>
2019-01-10 22:16:33 +0800

    最简单servlet项目

---
+++ src/servlet/ServletTest.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ServletTest extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
+		super.service(arg0, arg1);
+		arg1.getWriter().write("this servlet is start!");
+		System.out.println("this servlet is start!");
+	}
+		
+}
